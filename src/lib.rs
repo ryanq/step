afa851a036f1551a94def3bc5a7dc73df1b59f65
Ryan Quattlebaum <ryan.quattlebaum@icloud.com>
2017-02-11 20:10:44 -0500

    Add the Step trait
    The Step trait is for types that can be incremented or decremented by a
    unit value for the type or a given value.

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+/// Types that can be incremented or decremented by a unit value or a
+/// given value.
+pub trait Step {
+    /// Increment the value by the unit value for the type.
+    fn step(&self) -> Option<Self>;
+    /// Increment the value by a given value.
+    fn step_by(&self, by: &Self) -> Option<Self>;
+    /// Decrement the value by the unit value for the type.
+    fn step_back(&self) -> Option<Self>;
+    /// Decrement the value by a given value.
+    fn step_back_by(&self, by: &Self) -> Option<Self>;
+}
